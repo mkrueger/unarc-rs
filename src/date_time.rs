555ecfbee75f6862f128e4f30ccb13127bd5f9e0
Mike Krüger <mkrueger@posteo.de>
2025-03-17 10:06:00 +0100

    Added date/time conversion.

--- src/date_time.rs
+++ src/date_time.rs
@@ -30,3 +30,27 @@ impl DosDateTime {
         (self.0 & 0x1F) as u8 * 2
     }
 }
+
+impl Into<u32> for DosDateTime {
+    fn into(self) -> u32 {
+        self.0
+    }
+}
+
+impl From<u32> for DosDateTime {
+    fn from(date_time_modified: u32) -> Self {
+        Self(date_time_modified)
+    }
+}
+
+impl Into<(u16, u16)> for DosDateTime {
+    fn into(self) -> (u16, u16) {
+        ((self.0 >> 16) as u16, self.0 as u16)
+    }
+}
+
+impl From<(u16, u16)> for DosDateTime {
+    fn from((date, time): (u16, u16)) -> Self {
+        Self((date as u32) << 16 | time as u32)
+    }
+}
